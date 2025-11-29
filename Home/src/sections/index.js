5e7529081ac02d4de9635f28c8be37d0ecae902e
TPLink777 <stivengomezmazo7@gmail.com>
2025-11-28 22:48:14 -0500

    Arreglo en loc conflictos a la hora de desplegar

--- Home/src/sections/index.js
+++ Home/src/sections/index.js
@@ -4,4 +4,4 @@ export { default as Experiencia } from "./Experiencia/Experiencia.jsx";
 export { default as Seccion1 } from "./Sobre mi/Seccion1.jsx";
 export { default as Seccion2 } from "./Sobre mi/Seccion2.jsx";
 export { default as Seccion3 } from "./Sobre mi/Seccion3.jsx";
-export { default as Proyectos } from "./Proyectos/proyectos.jsx";
+export { default as Proyectos } from "./Proyectos/Proyectos.jsx";
